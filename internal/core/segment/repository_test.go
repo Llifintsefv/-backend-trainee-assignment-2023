183ce8cc9a9ec914543d302aa8d7d38ec70ddd1e
lifintsefv <157622370+Llifintsefv@users.noreply.github.com>
2024-12-20 17:19:54 +0300

    add repository test

--- internal/core/segment/repository_test.go
+++ internal/core/segment/repository_test.go
@@ -1,10 +1,7 @@
 package segment
 
 import (
-	"backend-trainee-assignment-2023/internal/core/models"
-	"database/sql"
 	"testing"
-	"time"
 )
 
 func TestSegmentRepository_GetUserSegments(t *testing.T) {
